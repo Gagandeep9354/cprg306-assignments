69ba7d50877faecc96c34e1cda53de380342908d
Gagandeep9354 <sgagan9354@gmail.com>
2024-09-10 15:35:05 -0600

    Initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import React from "react";
+function StudentInfo() {
+    return(
+        <>
+            <h1>Gagandeep Singh</h1>
+            GitHub Repository Link : <a href="https://github.com/Gagandeep9354/cprg306-assignments.git">cprg306-assignments</a>
+        </>
+    )
+}
+export default StudentInfo
