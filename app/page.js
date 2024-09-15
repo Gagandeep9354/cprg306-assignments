872634b06f5be7057883c2d1af880190f27d6fe6
Gagandeep9354 <sgagan9354@gmail.com>
2024-09-14 21:48:37 -0600

    Replaced anchor tags with Link component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import Link from 'next/link'
 
 function page() {
   return (
-    <>
+    <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href='./week-2'>page</a>
-    </>
+      <Link href="/week-2"> Week 2 Page </Link>
+    </div>
   )
 }
 
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Link from "next/link";
+
 function StudentInfo() {
     return(
         <>
             <h1>Gagandeep Singh</h1>
-            GitHub Repository Link : <a href="https://github.com/Gagandeep9354/cprg306-assignments.git">cprg306-assignments</a>
+            GitHub Repository Link : <Link href="https://github.com/Gagandeep9354/cprg306-assignments.git">cprg306-assignments</Link>
         </>
     )
 }
