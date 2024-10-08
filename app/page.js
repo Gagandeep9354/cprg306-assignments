8aa1d4dd793cd35e9820382f2b5d658de8e07dff
Gagandeep9354 <sgagan9354@gmail.com>
2024-10-08 15:29:34 -0600

    Added Link to week 4 page

--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ function page() {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2"> Week 2 Page </Link><br />
-      <Link href="/week-3"> Week 3 Page</Link>
+      <Link href="/week-3"> Week 3 Page</Link><br />
+      <Link href="/week-4"> Week 4 Page</Link>
     </div>
   )
 }
