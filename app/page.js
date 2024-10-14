c68a9a2a3856092edf8336b26188c17ed834f8f2
Gagandeep9354 <sgagan9354@gmail.com>
2024-10-14 03:01:54 -0600

    Week 5 complete

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ function page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <Link href="/week-2"> Week 2 Page </Link><br />
       <Link href="/week-3"> Week 3 Page</Link><br />
-      <Link href="/week-4"> Week 4 Page</Link>
+      <Link href="/week-4"> Week 4 Page</Link><br />
+      <Link href="/week-5"> Week 5 Page</Link>
     </div>
   )
 }
