df053860a22f1d7d732dda42bae77866f4b2e81f
Gagandeep9354 <sgagan9354@gmail.com>
2024-10-22 22:19:22 -0600

    Week 6 complete

--- app/page.js
+++ app/page.js
@@ -8,8 +8,9 @@ function page() {
       <Link href="/week-2"> Week 2 Page </Link><br />
       <Link href="/week-3"> Week 3 Page</Link><br />
       <Link href="/week-4"> Week 4 Page</Link><br />
-      <Link href="/week-5"> Week 5 Page</Link>
-    </div>
+      <Link href="/week-5"> Week 5 Page</Link><br />
+      <Link href="/week-6"> Week 6 Page</Link> 
+      </div>
   )
 }
 
